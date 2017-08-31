c003203209dfc302bd25449cf072bfe885e867ac
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-08-31 14:30:48 -0400

    Updated from Brython Server: 8/31/2017 2:30:48 PM

--- face.py
+++ face.py
@@ -1,1 +1,10 @@
+#Pedro Gallino
+#8/31/17
+#face.py - Make a face for proggramming porblem
 
+print('    xxxxx    ')
+print('   x     x   ')
+print('((   o o   ))')
+print('  |   v   |  ')
+print('  |  ===  |  ')
+print('    -----    ')
