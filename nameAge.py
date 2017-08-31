75573676be77029ecac9d83ae98d21cc53c0623a
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-08-31 14:53:35 -0400

    Updated from Brython Server: 8/31/2017 2:53:35 PM

--- nameAge.py
+++ nameAge.py
@@ -1,1 +1,11 @@
+#Pedro Gallino
+#8/31/17
+#nameAge.py - how to tell how many letters are in a name and how old you will be next year
 
+name = input('What is you name? ')
+age = int(input('How old are you? '))
+name1,name2 = name.split()
+print('Your name has', len(name),'letters')
+print('Your first name has',len(name1),'letters')
+print('Your last name has',len(name2),'letters')
+print('Next year you will be', age+1,'years old')
