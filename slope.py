8378eaca9d029f656acc6cf71dafccf534cae781
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-01 14:48:53 -0400

    Updated from Brython Server: 9/1/2017 2:48:53 PM

--- slope.py
+++ slope.py
@@ -1,1 +1,11 @@
+#Pedro Gallino
+#9/1/17
+#slope.py - Calculates slope after feeding in two points
 
+x1 = float(input('x1: '))
+y1 = float(input('y1: '))
+x2 = float(input('x2: '))
+y2 = float(input('y2: '))
+slope = ((y2-y1)/(x2-x1))
+print('Your slope is: ', slope)
+print('The equation for your line is: y=',slope,'x+',(y1-(slope*x1)))
