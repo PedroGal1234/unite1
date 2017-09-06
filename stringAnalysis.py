1d46b3cd15c6f29da8c3ee2353fcf5b4a09ca584
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-06 13:59:06 -0400

    Updated from Brython Server: 9/6/2017 1:59:06 PM

--- stringAnalysis.py
+++ stringAnalysis.py
@@ -2,4 +2,14 @@
 #9/1/17
 #stringAnalysis.py - discirbes a sentece
 
-input('Enter a Sentence: ')
+sentence = input('Enter a Sentence: ')
+sentence = sentence.lower()
+
+spaces = sentence.count(" ")
+characters = len(sentence)
+print('Your Sentence has',spaces+1,'words and',characters,'characters and',characters-(spaces),'letters.')
+
+sentence.lower()
+letter = input('Enter a Character to search for: ')
+print('Your sentence has', sentence.count(letter),'of the character: ',letter)
+word = input
