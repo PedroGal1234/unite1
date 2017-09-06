186dcfa38be38b3108373e4917c03272fca04154
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-06 13:34:28 -0400

    Updated from Brython Server: 9/6/2017 1:34:27 PM

--- additionGameDemo.py
+++ additionGameDemo.py
@@ -1,1 +1,10 @@
+#Pedro Gallino
+#9/6/17
+#additionGameDemo.py- our first "game"
+
+from random import randint
+num1 = randint(1,10)
+num2 =randint(1,10) 
+awnser = int(input((str(num1))+ '+'+str(num2)+' = '))
+print(awnser== num1 + num2)
 
