672b43c2c01e67e0b492d215dbbcc969e89df06e
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-07 14:54:26 -0400

    Updated from Brython Server: 9/7/2017 2:54:25 PM

--- isItATriangle.py
+++ isItATriangle.py
@@ -1,1 +1,10 @@
+#Pedro Gallino
+#9/7/17
+#isItATriangle.py- find out if a thing is a triangle
 
+a = float(input('Enter Side A: '))
+b = float(input('Enter Side B: '))
+c = float(input('Enter Side C: '))
+
+short = min(a,b,c)
+long = max(a,b,c)
