9df296dba7aed2215b1e4c057262c429bb4ab8fb
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-07 14:31:29 -0400

    Updated from Brython Server: 9/7/2017 2:31:28 PM

--- eggs.py
+++ eggs.py
@@ -1,1 +1,7 @@
+#Pedro Gallino
+#9/7/17
+#eggs.py- tells you if you can fut in a number of eggs into egg cartons
 
+eggs = int(input('Number of Eggs: '))
+carton = int(input('Number of Cartons: '))
+print(eggs<=(carton*12))
