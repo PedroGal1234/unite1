4daa6f79cc5db0a8d56625bdcc8119cc9316204b
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-07 14:22:42 -0400

    Updated from Brython Server: 9/7/2017 2:22:41 PM

--- warmUp2.py
+++ warmUp2.py
@@ -2,6 +2,7 @@
 #9/7/17
 #warmUp2.py - tells a lucky number after asking a name
 
-input('What is you name:  ')
 from random import randint
+
+input('What is you name:  ')
 print('Your Lucky Number is: ',randint(1,100))
