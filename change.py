8f7779c1c2a182c8f93e4fed6de320449e21513f
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-07 14:44:07 -0400

    Updated from Brython Server: 9/7/2017 2:44:06 PM

--- change.py
+++ change.py
@@ -1,1 +1,13 @@
+#Pedro Gallino
+#9/7/17
+#change.py-tells you the number of coins that will make the amount of money
 
+total = int(input('Eneter The Number of Cents: '))
+quarters = total//25
+print('Quarters: ',quarters)
+dimes = (total-(quarters*25))//10
+print('Dimes: ', dimes)
+nickle = (total-(quarters*25)-(dimes*10))//5
+print('Nickle: ', nickle)
+penny =(total-(quarters*25)-(dimes*10)-(nickle*5))//1
+print('Pennies: ',penny)
