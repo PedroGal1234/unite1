9a41fd0371fe03880da6362eb4b0158301c477a6
PedroGal1234 <31485824+PedroGal1234@users.noreply.github.com>
2017-09-11 14:18:53 -0400

    Updated from Brython Server: 9/11/2017 2:18:53 PM

--- quiz1.py
+++ quiz1.py
@@ -1,1 +1,11 @@
+#Pedro Gallino
+#9/11/17
+#quiz1.py - my first proggramming quiz
 
+print('Juan Pedro Gallino')
+num1 = int(input('Enter a number: '))
+num2 = int(input('Enter another number: '))
+
+print('The sum of your numbers is: ', num1+num2)
+print('The product of your numbers is: ', num1*num2)
+print('Your lucky number is:',(num1*7)+(num2*7))
